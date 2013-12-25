08f9bc8d2521addcde7f990394593609a7d87872
Lars Haugan <lars@larshaugan.net>
2013-12-26 00:24:12 +0100

    Added update functionality. Sorry for untested code in main. Updated READmE

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 dotfiles
 ========
 * Basic configuration for distributed user config
+* Easy to add new configuration (just add new .conf file in .config)
+
+
+Installation
+-------------
+    git clone https://github.con/larhauga/dotfiles.git ~/.config
+    sh dotfiles.sh install|rollback|update|remove
 
 Configuration
 -------------
-    git clone https://github.con/larhauga/dotfiles.git ~/.config
-    ln -s ~/.config/.bashrc ~/.bashrc
+Add new stuff to the dotfiles by adding or editing the files in .config/
+By adding a new file named .conf, will it be added when starting a shell
--- dotfiles.sh
+++ dotfiles.sh
@@ -7,7 +7,7 @@
 ####################
 
 help(){
-    echo "Usage $0 install|rollback|remove"
+    echo "Usage $0 install|rollback|update|remove"
     echo "Dotfiles script for installing, reverting or uninstalling the scripts"
 }
 
@@ -29,20 +29,29 @@ if [ $# -eq 1 ]; then
         rm $HOME/.tmux.conf
         ln -s $HOME/.config/bashrc ~/.bashrc
         ln -s $HOME/.config/tmux.conf ~/.tmux.conf
-
+        echo "New configuration installed"
     elif [ "$1" = "rollback" ]; then
         last_backup=`ls -t $backup_dir/ | head -1`
         echo "Rolling back to last install, $last_backup"
         rm $HOME/.bashrc
         rm $HOME/.tmux.conf
         cp $last_backup/* $HOME/.
-
+        echo "Config has been rolled back"
     elif [ "$1" = "remove" ]; then
         echo "Removing configuration and reverting to latest config"
         first_backup=`ls -t $backup_dir/ | tail -1`
         rm $HOME/.bashrc
         rm $HOME/.tmux.conf
         cp $first_backup/* $HOME/.
+        echo "Config has been removed"
+    elif [ "$1" = "update" ]; then
+        if [ ! -d $backup_dir ]; then
+            mkdir $backup_dir
+        fi
+        mkdir "$backup_dir/$time"
+        cp $HOME/.bashrc "$backup_dir/$time"
+        cp $HOME/.tmux.conf "$backup_dir/$time"
+        git pull origin master
     else
         help
     fi
