eea223cfb6edbc21b0e504f3394562a62b6f73ef
Lars Haugan <lars@larshaugan.net>
2014-01-06 01:41:57 +0100

    Fixed the configsync to work with pathing and not as function

--- bin/configsync.sh
+++ bin/configsync.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
+# Fast syncing configuration to other machines over ssh
+# Usefull for servers without direct internet connection
 
-configsync(){
-    if [ -z "$1" ]; then
-        echo "Usage: configsync.sh <host.domain.net>"
-    else
-        SERVER=$1
-        scp $HOME/.bashrc $SERVER:$HOME/.
-        scp -r $dotdir/. $SERVER:$dotdir/.
-        echo "Synced config"
-    fi
-}
+if [ -z "$1" ]; then
+    echo "Usage: configsync.sh <host.domain.net>"
+else
+    SERVER=$1
+    scp $HOME/.bashrc $SERVER:$HOME/.
+    scp -r $dotdir/* $SERVER:$dotdir/.
+    echo "Synced config"
+fi
